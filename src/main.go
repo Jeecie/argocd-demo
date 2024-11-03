36fa175a931ff7103b999f8cb5d25bfa4cab5054
Jeecie <jean.christophe.gauvreau@hotmail.com>
2024-11-03 09:59:17 -0500

    add HTTP server with hello handler in main.go

--- src/main.go
+++ src/main.go
@@ -1,8 +1,19 @@
 // main.go
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	http.HandleFunc("/hello", helloHandler)
+	fmt.Println("Starting server at port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
